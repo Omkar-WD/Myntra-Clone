dbad9afe14d2367b6faef7b2791f88c3aa348703
Omkar-WD <omkarpasalkar2020@gmail.com>
2022-01-18 23:42:34 +0530

    basic navigation bar design

---
+++ components/header.js
@@ -0,0 +1,58 @@
+function header() {
+  return `<div class="company-logo">
+    <img
+      src="https://logosarchive.com/wp-content/uploads/2021/12/Myntra-icon-logo.png"
+      alt=""
+    />
+  </div>
+  <div class="menu-options">
+    <div><a href="">MEN</a></div>
+    <div><a href="">WOMEN</a></div>
+    <div><a href="">KIDS</a></div>
+    <div><a href="">HOME & LIVING</a></div>
+    <div><a href="">BEAUTY</a></div>
+    <div><a href="">STUDIO</a></div>
+  </div>
+  <div class="search-bar">
+    <img
+      id="searchpng"
+      src="https://image.flaticon.com/icons/png/128/49/49116.png"
+      alt=""
+    />
+    <input type="text" placeholder="Search for products, brands and more" />
+  </div>
+  <div class="navbar-right">
+    <div>
+      <a href="">
+        <img
+          id="svg"
+          src="https://www.svgrepo.com/show/198180/user-profile.svg"
+          alt=""
+        />
+      </a>
+      <div>Profile</div>
+    </div>
+
+    <div>
+      <a href="">
+        <img
+          id="svg"
+          src="https://www.svgrepo.com/show/14970/heart.svg"
+          alt=""
+        />
+      </a>
+      <div>Wishlist</div>
+    </div>
+
+    <div>
+      <a href="">
+        <img
+          id="svg"
+          src="https://www.svgrepo.com/show/17522/bag.svg"
+          alt=""
+        />
+      </a>
+      <div>Bag</div>
+    </div>`;
+}
+export default header;
