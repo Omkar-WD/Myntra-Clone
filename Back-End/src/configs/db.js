720e3ff6a7dd274fa258411553c6df73f07a9d44
Omkar Pasalkar <omkarpasalkar2020@gmail.com>
2022-03-01 21:01:36 +0530

    ignore push

--- Back-End/src/configs/db.js
+++ Back-End/src/configs/db.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const dataBase = `mongodb+srv://${process.env.Mongo_Email}:${process.env.Mongo_Password}@cluster0.sthef.mongodb.net/${process.env.Mongo_DataBase}?retryWrites=true&w=majority`;
-const database = (module.exports = () => {
+module.exports = () => {
   return mongoose.connect(dataBase);
-});
+};
