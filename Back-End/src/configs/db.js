e571efd868ef3426195efb2f34b7a461f7cccb94
Omkar Pasalkar <omkarpasalkar2020@gmail.com>
2022-03-01 21:39:08 +0530

    Update db.js

--- Back-End/src/configs/db.js
+++ Back-End/src/configs/db.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 module.exports = () => {
   return mongoose.connect(
-    `mongodb+srv://omkar:15018000@cluster0.sthef.mongodb.net/myntraClone?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.Mongo_Email}:${process.env.Mongo_Password}@cluster0.sthef.mongodb.net/${process.env.Mongo_DataBase}?retryWrites=true&w=majority`
   );
 };
