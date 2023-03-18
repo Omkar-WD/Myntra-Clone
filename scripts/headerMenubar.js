636a7e2b7cb4bb7b25e40839de7e324049659664
Omkar Pasalkar <omkarpasalkar2020@gmail.com>
2023-03-18 10:44:42 +0530

    change backend url

--- scripts/headerMenubar.js
+++ scripts/headerMenubar.js
@@ -1,4 +1,4 @@
-var localhost = `https://myntra-backend-4kdj.onrender.com/`;
+var localhost = `https://myntra-backend-4kdj.onrender.com`;
 // var localhost = `http://localhost:2345`;
 function dropDown1() {
   let content = document.querySelector(".dropdown-content1");
